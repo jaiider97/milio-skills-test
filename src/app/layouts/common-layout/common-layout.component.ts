8a4f4caec1156715a13867d1b5aa4e62c622bcb1
jaider perez <jaider.perez@miliipay.com>
2024-11-14 14:13:50 -0500

    temporal

--- src/app/layouts/common-layout/common-layout.component.ts
+++ src/app/layouts/common-layout/common-layout.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
-    selector: 'app-common-layout',
-    templateUrl: './common-layout.component.html'
+  selector: 'app-common-layout',
+  templateUrl: './common-layout.component.html',
+  styleUrl: './common.layout.component.scss',
 })
-
-export class CommonLayoutComponent  {
-    constructor() { }
+export class CommonLayoutComponent {
+  cardList = [
+    {
+      title: 'Card 1',
+      description: 'This is a description for Card 1',
+      icon: 'bx-book-reader',
+      color: 'primary',
+    },
+    {
+      title: 'Card 2',
+    },
+    {
+      title: 'Card 3',
+    },
+    {
+      title: 'Card 4',
+    },
+    {
+      title: 'Card 5',
+    },
+    {
+      title: 'Card 6',
+    },
+  ]
+  constructor() {}
 }
